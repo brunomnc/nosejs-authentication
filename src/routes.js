b54f337ff8969906b4ffaf63f1930e87c35cc32c
ZummyGummy <brunomnc@yahoo.com.br>
2019-08-02 14:10:53 -0300

    MongoDB added

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,8 @@ import FileController from './app/controllers/FileController';
 import SessionController from './app/controllers/SessionController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/SchedulerController';
+
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -22,6 +24,8 @@ routes.get('/providers', ProviderController.index);
 
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
