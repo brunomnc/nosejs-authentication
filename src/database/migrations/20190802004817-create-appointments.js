a2b40f243bef98b414de131cfbe2f0c5f1816c57
ZummyGummy <brunomnc@yahoo.com.br>
2019-08-02 10:48:45 -0300

    Appointment creation functionality added

--- src/database/migrations/20190802004817-create-appointments.js
+++ src/database/migrations/20190802004817-create-appointments.js
@@ -8,13 +8,25 @@ module.exports = {
         primaryKey: true,
       },
       date: {
-        type: Sequelize.STRING,
+        type: Sequelize.DATE,
         allowNull: false,
       },
-      path: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true,
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      provider_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      canceled_at: {
+        type: Sequelize.DATE,
       },
       created_at: {
         type: Sequelize.DATE,
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import UserController from './app/controllers/UserController';
 import FileController from './app/controllers/FileController';
 import SessionController from './app/controllers/SessionController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -16,7 +17,11 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
